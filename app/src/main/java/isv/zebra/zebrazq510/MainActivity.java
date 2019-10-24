fbcf2548ed60b130bb4f8ce605e4b6f39dd38ecb
luisamilcarrr <luisamilcarr@gmail.com>
2019-10-24 12:36:08 -0600

    zebra

--- app/src/main/java/isv/zebra/zebrazq510/MainActivity.java
+++ app/src/main/java/isv/zebra/zebrazq510/MainActivity.java
@@ -226,7 +226,7 @@ public class MainActivity extends AppCompatActivity {
             if (printerLanguage == PrinterLanguage.ZPL) {
 
                 //para configurar un formato de impresion diseñarlo en la siguiente pagina http://labelary.com/viewer.html
-                
+
                 String bytes = "^XA^FX Top section with company logo, name and address.^CF0,60^FO50,50^GB100,100,100^FS^FO75,75^FR^GB100,100,100^FS^FO88,88^GB50,50,50^FS^FO220,50^FDIntershipping, Inc.^FS^CF0,30^FO220,115^FD1000 Shipping Lane^FS^FO220,155^FDShelbyville TN 38102^FS^FO220,195^FDUnited States (USA)^FS^FO50,250^GB700,1,3^FS^FX Second section with recipient address and permit information.^CFA,30^FO50,300^FDJohn Doe^FS^FO50,340^FD100 Main Street^FS^FO50,380^FDSpringfield TN 39021^FS^FO50,420^FDUnited States (USA)^FS^CFA,15^FO600,300^GB150,150,3^FS^FO638,340^FDPermit^FS^FO638,390^FD123456^FS^FO50,500^GB700,1,3^FS^FX Third section with barcode.^BY5,2,270^FO100,550^BC^FD12345678^FS^FX Fourth section (the two boxes on the bottom).^FO50,900^GB700,250,3^FS^FO400,900^GB1,250,3^FS^CF0,40^FO100,960^FDCtr. X34B-1^FS^FO100,1010^FDREF1 F00B47^FS^FO100,1060^FDREF2 BL4H8^FS^CF0,190^FO470,955^FDCA^FS^XZ";
 
                 configLabel = bytes.getBytes();
